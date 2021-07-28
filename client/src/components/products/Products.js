f4c39f420fd3abe63b78f0c80bd3f213d8338409
srhqmp <ballanosarah@gmail.com>
2021-07-28 16:56:34 +0800

    add pagination

--- client/src/components/products/Products.js
+++ client/src/components/products/Products.js
@@ -1,22 +1,57 @@
 import React, { useState, useEffect } from 'react'
 import './Products.css'
 
+
 import ProductList from './ProductList'
 import productService from '../../services/product'
 
 const Products = () => {
+  const [offset, setOffset] = useState(1)
   const [products, setProducts] = useState('')
+  const [perPage] = useState(6)
+  const [pageCount, setPageCount] = useState(0)
+
+  const getData = async () => {
+    const data = await productService.getAll()
+    const indexOfLast = offset * perPage
+    const indexOfFirst = indexOfLast - perPage
+    console.log(indexOfFirst, indexOfLast)
+    const slice = data.slice(indexOfFirst, indexOfLast)
+    console.log(slice)
+    setProducts(slice)
+    setPageCount(Math.ceil(data.length / perPage))
+  }
+
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected
+    setOffset(selectedPage + 1)
+  }
 
   useEffect(() => {
-    productService.getAll().then((data) => {
-      setProducts(data)
-    })
-  }, [])
+    getData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [offset])
 
   return (
-    <div className="products">
-      {products &&
-        products.map((item) => <ProductList key={item.id} item={item} />)}
+    <div>
+      <div className="products">
+        {products &&
+          products.map((item) => <ProductList key={item.id} item={item} />)}
+      </div>
+      <ReactPaginate
+        previousLabel={'<'}
+        nextLabel={'>'}
+        breakLabel={'...'}
+        breakClassName={'break-me'}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={'pagination'}
+        subContainerClassName={'pages pagination'}
+        activeClassName={'active'}
+        className="pagination"
+      />
     </div>
   )
 }
