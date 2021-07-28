67e8f099ea72d79b9528cb3c139efe8ad729edb2
srhqmp <ballanosarah@gmail.com>
2021-07-28 23:24:47 +0800

    modify grid

--- client/src/components/products/Products.js
+++ client/src/components/products/Products.js
@@ -5,7 +5,7 @@ import ProductList from './ProductList'
 const Products = ({ products }) => {
   return (
     <div>
-      <div className="products">
+      <div className="products grid gap-10 md:grid-cols-2 lg:grid-cols-3">
         {products &&
           products.map((item) => <ProductList key={item.id} item={item} />)}
       </div>
